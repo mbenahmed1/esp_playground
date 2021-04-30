d150d393785a997f49c12a4338dba1bbeee058cd
Mbenahmed <mbenahmed@uos.de>
2021-04-30 21:55:06 +0200

    upload data and plots

---
+++ eval/data/plots.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+sns.set_style("whitegrid")
+
+# load data from csv
+df = pd.read_csv("./complete.csv")
+
+# show first five entries
+print(df.head(5))
+
+# box plot
+bp = sns.catplot(x="distance", y="mbit/s", kind="box", hue="protocol", data=df)
+bp.set(xlabel="Abstand [m]", ylabel="Durchsatz [mbit/s]", title="Durchsatzmessung über 60 Sekunden")
+
+# line plot
+ln = sns.relplot(
+    data=df,
+    x="sec",
+    y="mbit/s",
+    hue="protocol",
+    style="distance",
+    kind="line",
+    markers=True   
+)
+ln.set(xlabel="Abstand [m]", ylabel="Durchsatz [mbit/s]", title="Durchsatzmessung über 60 Sekunden")
+
+# bar plot
+bar = sns.catplot(x="distance", y="mbit/s", hue="protocol", kind="bar", data=df)
+bar.set(xlabel="Abstand [m]", ylabel="Durchsatz [mbit/s]", title="Durchsatzmessung über 60 Sekunden")
+
+plt.show()
